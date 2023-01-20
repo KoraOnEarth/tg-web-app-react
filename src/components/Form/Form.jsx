d885b9fa44292a5af871cdf6a1fd980f7aab5afe
Nikita Smirnov <fiftikness1507@gmail.com>
2023-01-20 16:11:54 +0300

    aa

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -34,25 +34,7 @@ const Form = () => {
 
     return(
         <div className={'form'}>
-            <h3>Введите Ваши данные</h3>
-            <input
-             className={"input"}
-             type="text"
-             placeholder={'Страна'}
-             value={country}
-             onChange={onChangeCountry}
-            />
-            <input
-             className={"input"}
-             type="text"
-             placeholder={'Улица'}
-             value={street}
-             onChange={onChangeStreet}
-            />
-            <select value={subject} onChange={onChangeSubject} className={'select'}>
-                <option value={'physical'}>Физ. лицо</option>
-                <option value={'legal'}>Юр. лицо</option>
-            </select>
+            Form
         </div>
     )
 }
