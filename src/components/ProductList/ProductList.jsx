710f7207f151e52db386bdcd34c560e1ae715003
Nikita Smirnov <fiftikness1507@gmail.com>
2023-01-21 01:55:48 +0300

    init HOST

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://localhost:8000', {
+        fetch('http://109.71.13.222:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
